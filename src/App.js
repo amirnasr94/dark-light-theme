6858dbe47395ffbd4e9061db419eb65c1935c077
amirnasr <amir.nasr72@gmail.com>
2023-01-15 20:45:50 +0330

    config route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Layout from './components/layout/Layout'
+import {ThemeContext} from './contexts/theme'
 const App = () => {
+  // const {
+  //   isDark,
+  //   theme,
+  //   toggleTheme
+  // } = useContext(ThemeContext);
   return (
-    <div>App</div>
+    // <div className='app' style={{backgroundColor:theme.backgroundColor,color:theme.color}}>
+    //     <div className='text'>
+    //       it is a {isDark ? 'Dark' : 'light'} theme
+    //       <button onClick={toggleTheme}>toggle theme</button>
+    //     </div>
+    // </div>
+
+    <Routes>
+      <Route path='/' element={<Layout/>}>
+
+      </Route>
+    </Routes>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ThemeProvider from './contexts/ThemeProvider';
+import {BrowserRouter} from 'react-router-dom'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<ThemeProvider><App /></ThemeProvider>);
+root.render(<BrowserRouter><App /></BrowserRouter>);
