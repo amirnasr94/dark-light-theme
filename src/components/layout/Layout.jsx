e1c885b613f881d36b95a3439b11f4241d3d65cd
amirnasr <amir.nasr72@gmail.com>
2023-01-15 19:51:40 +0330

    create layout component

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+const Layout = () => {
+
+  return (
+    <div className='wrapper'>
+        <div className='layoat-sidebar'>
+
+        </div>
+        <div>
+            <div className='layoat-body-header'>
+
+            </div>
+            <hr className='devider' size="4"/>
+            <div className='layoat-body-content'>
+            
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Layout
