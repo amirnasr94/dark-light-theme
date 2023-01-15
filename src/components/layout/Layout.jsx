d3f84e5ffe45b968283a0dbb03da04677510b89c
amirnasr <amir.nasr72@gmail.com>
2023-01-15 20:45:06 +0330

    edit layout component

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,19 +1,33 @@
 import React, { useState } from 'react'
+import Sidebar from '../sidebar/Sidebar'
 
 const Layout = () => {
-
+  const [showSidebar,setShowSidebar] = useState(false);
+  const handleShowSidebar = (x) => {
+      if (x !== null) {
+          setShowSidebar(x)
+      }else{
+          setShowSidebar(!showSidebar);
+      }
+  }
   return (
     <div className='wrapper'>
-        <div className='layoat-sidebar'>
-
-        </div>
-        <div>
-            <div className='layoat-body-header'>
-
+        <div className='layoat-container'>
+            <div className={showSidebar ? "layoat-sidebar sidebar-inactive" : "layoat-sidebar"}>
+                <Sidebar showSidebar={showSidebar} handleShowSidebar={handleShowSidebar}/>
             </div>
-            <hr className='devider' size="4"/>
-            <div className='layoat-body-content'>
-            
+            <div className={showSidebar ? "layoat-body layoat-body-inactive" : "layoat-body"}>
+                <div className='layoat-body-header'>
+                    {/* <Header/> */}
+                </div>
+                <hr className='devider' size="4"/>
+                <div className='layoat-body-content'>
+                    {/* <Outlet/> */}
+
+                    <div className='layoat-footer'>
+                        <img src='../assets/images/rectangle.png' alt=''/>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
