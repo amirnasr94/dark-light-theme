26450185007f43a591c4683b2909a55ffbad4725
amirnasr <amir.nasr72@gmail.com>
2023-01-15 20:45:35 +0330

    add theme context

--- src/contexts/ThemeProvider.jsx
+++ src/contexts/ThemeProvider.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ThemeContext } from './theme'
 
 const ThemeProvider = ({children}) => {
+  const [isDark,setIsDark] = useState(false);
+
+  const themes = {
+    dark:{
+        backgroundColor: '#000',
+        color:'#fff'
+    },
+    light:{
+        backgroundColor:'#fff',
+        color:'#000'
+    }
+
+  }
+
+  const theme = isDark ? themes.dark : themes.light;
+
+  const toggleTheme = () => {
+    localStorage.setItem("isDark",JSON.stringify(!isDark));
+    console.log(JSON.stringify(!isDark));
+    setIsDark(prevState => !prevState);
+  }
+
+  useEffect(() => {
+    const isDark = localStorage.getItem("isDark") === "true";
+    setIsDark(isDark)
+  },[]);
+  
   return (
-    <ThemeContext.Provider value={{}}>
+    <ThemeContext.Provider value={{
+      isDark,
+      theme,
+      toggleTheme
+    }}>
         {children}
     </ThemeContext.Provider>
   )
--- src/contexts/theme.js
+++ src/contexts/theme.js
@@ -1,5 +1,10 @@
 import { createContext } from "react";
 
+
+
 export const ThemeContext = createContext({
+    isDark:false,
+    theme:{},
+    toggleTheme: () => {}
     
 })
